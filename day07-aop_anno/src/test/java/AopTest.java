5d30716f0fada148f38fd3cdcf43a44169ed5252
sunbikai <39723342+sunbikai@users.noreply.github.com>
2019-08-03 16:27:45 +0800

    Update AopTest.java

--- day07-aop_anno/src/test/java/AopTest.java
+++ day07-aop_anno/src/test/java/AopTest.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
+//在git上添加这段字符
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:springaop.xml")
 public class AopTest {
